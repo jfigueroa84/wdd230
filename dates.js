a56cd52a5134d31ddd87e65e00cd466ce7e39a8f
jfigueroa84 <fig21004@byui.edu>
2022-01-20 08:02:20 -0800

    Delete dates.js

--- dates.js
+++
@@ -1,5 +0,0 @@
-const lastmod = document.querySelector('#lastmod');
-lastmod.textContent = `Last modified: ${document.lastModified}`;
-
-const currentDate = new Date();
-document.querySelector("#year").textContent = currentDate.getFullYear();
