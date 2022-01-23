215104fdd19bff4bd1939fd53561c75a659d70f7
jfigueroa84 <fig21004@byui.edu>
2022-01-22 22:09:25 -0800

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -1,5 +1,5 @@
 const lastmod = document.querySelector('#lastmod');
-lastmod.textContent = `Last modified: ${document.lastModified}`;
+lastmod.textContent = `Last Modified: ${document.lastModified}`;
 
 const currentDate = new Date();
 document.querySelector("#year").textContent = currentDate.getFullYear();
