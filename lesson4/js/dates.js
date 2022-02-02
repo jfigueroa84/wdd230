221cbe7af909187117abe16d738d6a358aab7049
jfigueroa84 <fig21004@byui.edu>
2022-02-02 07:21:12 -0800

    Update dates.js

--- lesson4/js/dates.js
+++ lesson4/js/dates.js
@@ -3,3 +3,30 @@ lastmod.textContent = `Last Modified: ${document.lastModified}`;
 
 const currentDate = new Date();
 document.querySelector("#year").textContent = currentDate.getFullYear();
+
+
+
+
+{
+const openMenuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav");
+
+openMenuToggle.addEventListener(
+    "click",
+    () => {
+        navMenu.classList.toggle("open");
+        openMenuToggle.innerHTML = navMenu.classList.contains("open")
+        ? "&#10005; Close"
+        : "&#9776; Menu";
+    },
+    false
+  );
+  window.onresize = () => {
+    if (window.innerWidth > 761) {
+      navMenu.classList.remove("open");
+      openMenuToggle.innerHTML = '&#9776; Menu';
+    }
+};
+}
+window.onload = init;
+
