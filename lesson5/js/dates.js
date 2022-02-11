24ef855bb2189afe8272ec487ba1a401a7acb5c7
jfigueroa84 <fig21004@byui.edu>
2022-02-11 09:07:31 -0800

    Create dates.js

---
+++ lesson5/js/dates.js
@@ -0,0 +1,51 @@
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = `Last Modified: ${document.lastModified}`;
+
+const currentDate = new Date();
+document.querySelector("#year").textContent = currentDate.getFullYear();
+
+
+
+
+function init() {
+  // current date
+  document.querySelector('.today').innerHTML = new Intl.DateTimeFormat(
+    'en-US',
+    {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }
+  ).format(new Date());
+  const isMonday = new Date().getDay() === 5;
+  if (isMonday) {
+    document.querySelector('.announcement-banner').style.display = 'block';
+  }
+}
+
+
+
+const openMenuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav");
+
+openMenuToggle.addEventListener(
+    "click",
+    () => {
+        navMenu.classList.toggle("open");
+        openMenuToggle.innerHTML = navMenu.classList.contains("open")
+        ? "&#10005; Close"
+        : "&#9776; Menu";
+    },
+    false
+  );
+  window.onresize = () => {
+    if (window.innerWidth > 761) {
+      navMenu.classList.remove("open");
+      openMenuToggle.innerHTML = '&#9776; Menu';
+    }
+};
+
+
+window.onload = init;
+
