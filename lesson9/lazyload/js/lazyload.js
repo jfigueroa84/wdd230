970dc8e89c9de3a2bdeffee892493e7b017d2d89
jfigueroa84 <fig21004@byui.edu>
2022-03-04 20:06:58 -0800

    week9_first

---
+++ lesson9/lazyload/js/lazyload.js
@@ -0,0 +1,36 @@
+function init() {
+  const imgOptions = {
+    threshold: 0.20,
+    
+  };
+
+  const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+  const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+      image.removeAttribute('data-src');
+    };
+  };
+
+  if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+}
+
+window.addEventListener('load', init);
